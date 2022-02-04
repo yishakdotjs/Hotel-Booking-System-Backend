a085c9c774b4cf03728e826dabeca8abdbdaf717
Yishak Abraham <yishakmain@gmail.com>
2022-02-04 16:41:31 +0300

    Test

--- config/db.js
+++ config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 const config = require("config");
-const db = config.get("mongoURLLocal");
+const db = config.get("mongoURLAtlas");
 
 mongoose.Promise = global.Promise;
 
