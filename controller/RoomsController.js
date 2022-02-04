eefb8ec0fd41e59caba8967bced3edde89d321c0
Yishak Abraham <yishakmain@gmail.com>
2022-02-04 15:04:45 +0300

    Auth Completed

--- controller/RoomsController.js
+++ controller/RoomsController.js
@@ -1,17 +1,15 @@
-const roomModel = require ('../model/room')
+const roomModel = require("../model/room");
 
 //const router = require ('../routes/api/roomsRoute');
 
-
-const RoomsController ={
-    getRooms: async(req, res) => 
-    {
-        try {
-            const rooms = await roomModel.find({})
-            return res.json({ rooms });
-        }catch (error) {
-            return res.status(400).json({ message: error });
-        }
-    },
-}
-module.exports =  RoomsController;
+const RoomsController = {
+  getRooms: async (req, res) => {
+    try {
+      const rooms = await roomModel.find({});
+      return res.json({ rooms });
+    } catch (error) {
+      return res.status(400).json({ message: error });
+    }
+  },
+};
+module.exports = RoomsController;
